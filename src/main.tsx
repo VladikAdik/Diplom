695250b36e712d2b09fcea1571c544b575c8e05b
VladikAdik <ralf.chishko2@yandex.ru>
2026-04-12 12:24:19 +0300

    Start point of the project

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import { App } from './App.tsx'
+
+const rootEl = document.getElementById("root")
+const reactRoot = createRoot(rootEl!)
+reactRoot.render(<App />)
