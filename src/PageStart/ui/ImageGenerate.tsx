62f606976cd5fae57983aca14cb79b7da1ee0b12
VladikAdik <ralf.chishko2@yandex.ru>
2026-05-02 16:01:24 +0300

    Beta build v2

--- src/PageStart/ui/ImageGenerate.tsx
+++ src/PageStart/ui/ImageGenerate.tsx
@@ -8,8 +8,8 @@ export function ImageGenerator() {
   const [generatedImage, setGeneratedImage] = useState<string | null>(null);
   const [error, setError] = useState('');
   const [ratio, setRatio] = useState('1:1')
-  const apiSD = "http://localhost:80";
-  const apiLum = "http://localhost:8000";
+  const apiSD = "https://diplom-backend-sd.cloudpub.ru";
+  const apiLum = "https://diplom-backend-lum.cloudpub.ru";
   const apiSd3 = "https://api.stability.ai/v2beta/stable-image/generate/sd3";
   let height: number;
   let width: number;
@@ -18,37 +18,38 @@ export function ImageGenerator() {
     setPrompt(event.target.value);
     setError(''); // Очищаем ошибку при вводе
   };
-  
+
   const handleGenerate = async () => {
     if (!prompt.trim()) {
       setError('Пожалуйста, введите промпт');
       return;
     }
     switch (ratio) {
-    case '1:1':
-      height = 512;
-      width = 512;
-      break;
-    case '16:9':
-      height = 512;
-      width = 912;
-      break;
-    case '9:16':  
-      height = 912;
-      width = 512;
-      break;
-  }
+      case '1:1':
+        height = 512;
+        width = 512;
+        break;
+      case '16:9':
+        height = 512;
+        width = 912;
+        break;
+      case '9:16':
+        height = 912;
+        width = 512;
+        break;
+    }
     setIsLoading(true);
     setError('');
     try {
       const requestData = {
-        prompt: prompt, // Текстовое описание
+        prompt: prompt,
         "negative_prompt": "",
         "height": height,
         "width": width,
         "steps": 20,
-        "guidance_scale": 7.5              // Количество изображений для генерации
+        "guidance_scale": 7.5
       };
+      
       const response = await fetch(modelUrl + "/generate", {
         method: 'POST',
         headers: {
@@ -64,7 +65,7 @@ export function ImageGenerator() {
       const data = await response.json();
       const imUrl = modelUrl + data.url;
       setGeneratedImage(imUrl); // предполагаем, что сервер возвращает URL изображения
-      
+
     } catch (err) {
       const errorMessage = err instanceof Error ? err.message : 'Произошла неизвестная ошибка';
       setError(errorMessage);
@@ -86,18 +87,18 @@ export function ImageGenerator() {
         prompt: prompt, // Текстовое описание
         "negative_prompt": "",
         "aspect_ratio": ratio,
-        "model": "sd3.5-flash"         
+        "model": "sd3.5-flash"
       };
-      const response = await axios.postForm(modelUrl, axios.toFormData(requestData, new FormData()),{
-        headers: { 
-        Authorization: `Bearer sk-m1NS0X6wv8yGnWJv2WXiVCVVcAsF0YFS8mifJtmJb9le0oYp`, //sk-nwCTKQpjrSM08sob4ykrsMCIZypJlWHAJxdaad14nk2u9vJj sk-m1NS0X6wv8yGnWJv2WXiVCVVcAsF0YFS8mifJtmJb9le0oYp
-        Accept: "application/json" 
+      const response = await axios.postForm(modelUrl, axios.toFormData(requestData, new FormData()), {
+        headers: {
+          Authorization: `Bearer sk-m1NS0X6wv8yGnWJv2WXiVCVVcAsF0YFS8mifJtmJb9le0oYp`, //sk-nwCTKQpjrSM08sob4ykrsMCIZypJlWHAJxdaad14nk2u9vJj sk-m1NS0X6wv8yGnWJv2WXiVCVVcAsF0YFS8mifJtmJb9le0oYp
+          Accept: "application/json"
         },
       }
       );
       const imageBase64 = response.data.image;
       setGeneratedImage(imageBase64); // предполагаем, что сервер возвращает URL изображения
-      
+
     } catch (err) {
       const errorMessage = err instanceof Error ? err.message : 'Произошла неизвестная ошибка';
       setError(errorMessage);
@@ -113,16 +114,19 @@ export function ImageGenerator() {
       modelUrl === apiSd3 ? handleGenerateCloud : handleGenerate;
     }
   };
-const models = [
-  { value: apiSD, label: 'Stable-diffusion-1.5' },
-  { value: apiLum, label: 'Lumina' },
-  { value: apiSd3, label: 'Stable-diffusion-3.5'},
-];
-const aspect = [
-  { value: '1:1', label: '1:1' },
-  { value: '16:9', label: '16:9' },
-  { value: '9:16', label: '9:16'},
-];
+
+  const models = [
+    { value: apiSD, label: 'Stable-diffusion-1.5' },
+    { value: apiLum, label: 'Lumina' },
+    { value: apiSd3, label: 'Stable-diffusion-3.5' },
+  ];
+
+  const aspect = [
+    { value: '1:1', label: '1:1' },
+    { value: '16:9', label: '16:9' },
+    { value: '9:16', label: '9:16' },
+  ];
+
   return (
     <div>
       <div>
@@ -134,8 +138,8 @@ const aspect = [
           rows={4}
           disabled={isLoading}
         />
-        
-        <button 
+
+        <button
           onClick={modelUrl === apiSd3 ? handleGenerateCloud : handleGenerate}
           disabled={isLoading || !prompt.trim()}
         >
@@ -165,8 +169,8 @@ const aspect = [
       {generatedImage && (
         <div>
           <h3>Результат генерации:</h3>
-          <img 
-            src={generatedImage} 
+          <img
+            src={generatedImage}
             alt="Сгенерированное изображение"
           />
         </div>
