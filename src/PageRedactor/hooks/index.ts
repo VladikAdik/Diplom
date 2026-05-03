960229de5131f911427ad94726e4efbc04ea9966
VladikAdik <ralf.chishko2@yandex.ru>
2026-05-03 22:01:08 +0300

    Little fix

--- src/PageRedactor/hooks/index.ts
+++ src/PageRedactor/hooks/index.ts
@@ -2,6 +2,5 @@
 export { useLayers, useLayerSelection, useLayerPosition } from './layers';
 export { useCropTool, useFilters, useDrawingTool } from './tools';
 export type { FilterType } from './tools';
-export { usePopover, useSelectionRect, useSnapMove } from './interaction';
-export type { SnapGuide } from './interaction';
+export { usePopover, useSelectionRect } from './interaction';
 export { useHistory, useStageSize, useWorkspaceLogic } from './workspace';
